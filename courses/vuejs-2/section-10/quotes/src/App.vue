a5821762e82bcdc11f77af604c707069049f5c25
Cássio Farias Machado <cassiofariasmachado@yahoo.com>
2018-03-31 14:06:14 -0300

    Adicionar sessão 10 do curso de VueJS

---
+++ courses/vuejs-2/section-10/quotes/src/App.vue
@@ -0,0 +1,50 @@
+<template>
+    <div class="container">
+      <header-card :quote-count="quotes.length" :max-quotes="maxQuotes"></header-card>
+      <new-quote @quoteCreated="addQuote"></new-quote>
+      <quote-grid :quotes="quotes" @quoteDeleted="removeQuote"></quote-grid>
+      <div class="row">
+        <div class="col-sm-12 text-center">
+          <div class="alert alert-info">
+            Info: Click on a quote to delete it!
+          </div>
+        </div>
+      </div>
+    </div>
+</template>
+
+<script>
+import QuoteGrid from './components/QuoteGrid.vue';
+import NewQuote from './components/NewQuote.vue';
+import HeaderCard from './components/Header.vue';
+
+export default {
+  data: function() {
+    return {
+      quotes: ['Just a Quote to see something'],
+      maxQuotes: 10
+    };
+  },
+  methods: {
+    addQuote(quote) {
+      if (this.quotes.length >= this.maxQuotes) {
+        return alert('Please, delete quotes first!');
+      }
+
+      this.quotes.push(quote);
+    },
+    removeQuote(index) {
+      this.quotes.splice(index, 1);
+    }
+  },
+  components: {
+    QuoteGrid,
+    NewQuote,
+    HeaderCard
+  }
+};
+</script>
+
+<style>
+
+</style>
---
+++ courses/vuejs-2/section-10/quotes/src/components/QuoteGrid.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="row">
+        <quote v-for="(quote, index) in quotes" :key="index" @click.native="deleteQuote(index)">
+            {{ quote }}
+        </quote>
+    </div>
+</template>
+
+<script>
+import Quote from './Quote.vue';
+
+export default {
+  components: {
+    Quote
+  },
+  props: {
+    quotes: Array
+  },
+  methods: {
+    deleteQuote(index) {
+      this.$emit('quoteDeleted', index);
+    }
+  }
+};
+</script>
+
+<style scoped>
+.panel-body {
+  font-family: 'Arizonia', cursive;
+  font-size: 24px;
+  color: #6e6e6e;
+}
+
+.quote {
+  cursor: pointer;
+}
+
+.quote:hover {
+  background-color: #ffe2e2;
+}
+</style>
